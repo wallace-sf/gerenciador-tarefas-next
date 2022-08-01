10f144a9867357274f8528d2c0d646377afe007e
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-01 20:41:28 -0300

    feat: add

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,45 @@
-import { DefaultMsgResponse } from "./../../types/DefaultMsgResponse";
-import type { NextApiRequest, NextApiResponse } from "next";
+import md5 from 'md5';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { connectDb } from '../../middlewares/connectDb';
+import { UserModel } from '../../models/UserModel';
+import { DefaultResponseMsg } from '../../types/DefaultResponseMsg';
+import { LoginRequest } from '../../types/LoginRequest';
+import jwt from 'jsonwebtoken';
+import { LoginResponse } from '../../types/LoginResponse';
 
-const store = (
-  req: NextApiRequest,
-  res: NextApiResponse<DefaultMsgResponse>
-) => {
-  if (req.method === "POST") {
-    const { login, password } = req.body;
-    if (login === "rafael@verzel.com.br" && password === "Senha@123") {
-      return res.status(200).json({ msg: "Login autenticado!" });
+const loginEndpoint = async(req : NextApiRequest, 
+    res : NextApiResponse<DefaultResponseMsg | LoginResponse>) => {
+
+    const {MY_SECRET_KEY} = process.env;
+    if(!MY_SECRET_KEY){
+        return res.status(500).json({ error : "ENV Chave JWT não informada"});
+    }
+    
+
+    if(req.method === 'POST'){
+        const body = req.body as LoginRequest;
+        if(!body || !body.login || !body.password){
+            return res.status(400).json({ error : 'Favor informar usuário e senha'});
+        }
+
+        const usersFound = await UserModel.find({ email : body.login, password : md5(body.password)});
+        if( usersFound && usersFound.length > 0 ){
+            const user = usersFound[0];
+            const token = jwt.sign({_id : user._id}, MY_SECRET_KEY);
+            
+            const result = {
+                name : user.name,
+                email : user.email,
+                token
+            }
+
+            return res.status(200).json(result);
+        }
+
+        return res.status(400).json({ error : 'Usuário ou senha não encontrado'});
     }
 
-    return res.status(400).json({ error: "Credenciais inválidas!" });
-  }
-  return res.status(405).json({ error: "Metodo informado nao é permitido!" });
-};
+    return res.status(405).json({ error : 'Metodo infomado não é valido'});
+}
 
-export default store;
+export default connectDb(loginEndpoint);
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,39 +1,43 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { DefaultResponseMsg } from '../../types/DefaultResponseMsg';
+import { UserRequest } from '../../types/UserRequest';
+import { connectDb} from '../../middlewares/connectDb';
+import md5 from 'md5';
+import { UserModel } from '../../models/UserModel';
 
-import { UserModel } from "../../models/UserModel";
-import { connect } from "../../middlewares/connectToMongoDB";
+const userEndpoint = async (req : NextApiRequest, res : NextApiResponse<DefaultResponseMsg>) => {
 
-const registerEndpoint = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    if (req.method === "POST") {
-      const { name, email = "", password } = req.body;
+    if(req.method === 'POST'){
+        const body = req.body as UserRequest;
 
-      if (!name || name.trim().length < 2) {
-        return res.status(400).json({ error: "Nome não é válido" });
-      }
+        if(!body.name || body.name.length < 2 ){
+            return res.status(400).json({ error : 'Nome inválido'});
+        }
 
-      if (!email || email.trim().length < 5) {
-        return res
-          .status(400)
-          .json({ error: "A senha deve ter pelo menos 5 caracteres." });
-      }
+        if(!body.email || body.email.length < 5 ){
+            return res.status(400).json({ error : 'Email inválido'});
+        }
 
-      if (!email.includes("@") || !email.includes(".")) {
-        return res
-          .status(400)
-          .json({ error: "Formato de e-mail não é válido" });
-      }
+        if(!body.password || body.password.length < 4 ){
+            return res.status(400).json({ error : 'Senha inválida'});
+        }
 
-      await UserModel.create({ name, email, password });
+        const existingUserWithEmail = await UserModel.find({email : body.email});
+        if(existingUserWithEmail && existingUserWithEmail.length){
+            return res.status(400).json({ error : 'Já existe usuário com o email informado'});
+        }
 
-      return res.status(200).json({ msg: "Usuário cadastrado com sucesso!" });
+        const user = {
+            name : body.name,
+            email : body.email,
+            password : md5(body.password)
+        }
+
+        await UserModel.create(user);
+        return res.status(200).json({ msg : 'Usuario Criado'});
     }
-  } catch (error) {
-    console.error("Error on create user", error);
-    return res.status(500).json({
-      error: "Não foi possível cadastrar o usuário, entre em contato.",
-    });
-  }
-};
 
-export default connect(registerEndpoint);
+    return res.status(405).json({ error : 'Metodo infomado não é valido'});
+}
+
+export default connectDb(userEndpoint);
